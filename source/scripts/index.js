2f70aef63192c6b316045d22cbd33e4e233006f2
Yuriy Bevov <jury19y@mail.ru>
2020-07-08 23:59:12 +0300

    добавил aos и lazyload

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -16,3 +16,22 @@ document.addEventListener('DOMContentLoaded', function(){
     itemSelector: '.grid-item',
   });
 });
+
+import 'lazysizes';
+// import a plugin
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+import AOS from 'aos';
+//import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+AOS.init({
+  //offset: 120,
+  startEvent: 'DOMContentLoaded',
+  delay: 500,
+  duration: 1500,
+  once: true,
+  anchorPlacement: 'bottom-center',
+
+  //anchorPlacement: 'center-bottom',
+  //useClassNames: true
+});
