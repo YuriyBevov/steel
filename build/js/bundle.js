f3bca1cd51b448070d5ae9e1c876a6c897a976df
Yuriy Bevov <jury19y@mail.ru>
2020-07-23 20:07:20 +0300

    изменил анимация заголовка

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -13073,7 +13073,7 @@ const numberAnimation = function () {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-const showIntroTitle = () => {
+/*const showIntroTitle = () => {
   const intro = document.querySelector('.intro__description');
 
   if(intro) {
@@ -13087,6 +13087,38 @@ const showIntroTitle = () => {
       path[i].setAttribute('stroke-dashoffset', pathLength);
       path[i].classList.add('start-animation');
     }
+  }
+};*/
+
+const showIntroTitle = () => {
+
+  const text = document.querySelector('.intro__description');
+  const strText = text.textContent;
+  const splitText = strText.split('');
+
+  text.textContent = '';
+
+  for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>"
+  }
+
+  let char = 0;
+
+  let timer= setInterval(onTick, 50);
+
+  function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splitText.length) {
+      complete();
+      return;
+    }
+  }
+
+  function complete(){
+    clearInterval(timer);
+    timer = null;
   }
 };
 
--- source/scripts/modules/showIntroTitle.js
+++ source/scripts/modules/showIntroTitle.js
@@ -1,4 +1,4 @@
-const showIntroTitle = () => {
+/*const showIntroTitle = () => {
   const intro = document.querySelector('.intro__description');
 
   if(intro) {
@@ -12,6 +12,38 @@ const showIntroTitle = () => {
       path[i].setAttribute('stroke-dashoffset', pathLength);
       path[i].classList.add('start-animation');
     }
+  }
+};*/
+
+const showIntroTitle = () => {
+
+  const text = document.querySelector('.intro__description');
+  const strText = text.textContent;
+  const splitText = strText.split('');
+
+  text.textContent = '';
+
+  for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>"
+  }
+
+  let char = 0;
+
+  let timer= setInterval(onTick, 50);
+
+  function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splitText.length) {
+      complete();
+      return;
+    }
+  }
+
+  function complete(){
+    clearInterval(timer);
+    timer = null;
   }
 };
 
