cb104f3cb7e570a2f5345bef487f26e472480f5e
Yuriy Bevov <jury19y@mail.ru>
2020-08-02 16:46:07 +0300

    убрал баг слайдера

--- source/scripts/modules/swiper.js
+++ source/scripts/modules/swiper.js
@@ -1,97 +1,99 @@
 import Swiper from "swiper";
 
 export const slidersInit = function () {
-  const sliderDelay = 3000;
+  document.addEventListener("DOMContentLoaded", function() {
+    const sliderDelay = 3000;
 
-  let pSlider = document.querySelector('.partnership__swiper-container');
+    let pSlider = document.querySelector('.partnership__swiper-container');
 
-  if(pSlider) {
-    let partnershipSwiper = new Swiper('.partnership__swiper-container', {
-      slidesPerView: 3,
-      slidesPerColumn: 3,
-      autoplay: {
-        delay: sliderDelay,
-      },
-      navigation: {
-        nextEl: '.p-swiper-button-next',
-        prevEl: '.p-swiper-button-prev',
-      },
-    });
-  }
-
-  let aSlider = document.querySelector('.advantages__swiper-container');
-
-  if(aSlider) {
-    let advantagesSwiper = new Swiper('.advantages__swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      initialSlide: 1,
-      centeredSlides: true,
-      slideToClickedSlide: true,
-      navigation: {
-        nextEl: '.a-swiper-button-next',
-        prevEl: '.a-swiper-button-prev',
-      },
-      pagination: {
-        el: '.a-swiper-pagination',
-        dynamicBullets: true,
-      },
-      breakpoints: {
-        // when window width is >= 836px
-        514: {
-          slidesPerView: 2,
-          centeredSlides: false,
+    if(pSlider) {
+      let partnershipSwiper = new Swiper('.partnership__swiper-container', {
+        slidesPerView: 3,
+        slidesPerColumn: 3,
+        autoplay: {
+          delay: sliderDelay,
         },
-
-        768: {
-          slidesPerView: 2,
-          centeredSlides: false,
+        navigation: {
+          nextEl: '.p-swiper-button-next',
+          prevEl: '.p-swiper-button-prev',
         },
-        // when window width is >= 1160px
-        1160: {
-          slidesPerView: 3,
-          centeredSlides: true,
-          initialSlide: 2,
+      });
+    }
+
+    let aSlider = document.querySelector('.advantages__swiper-container');
+
+    if(aSlider) {
+      let advantagesSwiper = new Swiper('.advantages__swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        initialSlide: 1,
+        centeredSlides: true,
+        slideToClickedSlide: true,
+        navigation: {
+          nextEl: '.a-swiper-button-next',
+          prevEl: '.a-swiper-button-prev',
+        },
+        pagination: {
+          el: '.a-swiper-pagination',
+          dynamicBullets: true,
+        },
+        breakpoints: {
+          // when window width is >= 836px
+          514: {
+            slidesPerView: 2,
+            centeredSlides: false,
+          },
+
+          768: {
+            slidesPerView: 2,
+            centeredSlides: false,
+          },
+          // when window width is >= 1160px
+          1160: {
+            slidesPerView: 3,
+            centeredSlides: true,
+            initialSlide: 2,
+          }
         }
-      }
-    });
-  }
+      });
+    }
 
-  let fSlider = document.querySelectorAll('.furniture-swiper-container');
+    let fSlider = document.querySelectorAll('.furniture-swiper-container');
 
-  if(fSlider) {
+    if(fSlider) {
 
-    fSlider.forEach(eachSlider => new Swiper(fSlider, {
-      spaceBetween: 30,
-      slidesPerView: 'auto',
-      freeMode: true,
+      fSlider.forEach(eachSlider => new Swiper(fSlider, {
+        spaceBetween: 30,
+        slidesPerView: 'auto',
+        freeMode: true,
 
-      pagination: {
-        el: '.f-swiper-pagination',
-        type: 'progressbar',
-      },
-      navigation: {
-        nextEl: '.f-swiper-button-next',
-        prevEl: '.f-swiper-button-prev',
-      },
-    }))
-  }
+        pagination: {
+          el: '.f-swiper-pagination',
+          type: 'progressbar',
+        },
+        navigation: {
+          nextEl: '.f-swiper-button-next',
+          prevEl: '.f-swiper-button-prev',
+        },
+      }))
+    }
 
-  let partSlider = document.querySelector('.partitions-swiper-container');
+    let partSlider = document.querySelector('.partitions-swiper-container');
 
-  if(partSlider) {
-    let partitionsSlider = new Swiper(partSlider, {
-      slidesPerView: 'auto', // записать в пометки , как способ ограничивать контэйнер при свободном режиме при разной ширине слайдов, сэкономит кучу времени !!!!!!!!!!!!
-      spaceBetween: 30,
-      freeMode: true,
-      pagination: {
-        el: '.part-swiper-pagination',
-        type: 'progressbar',
-      },
-      navigation: {
-        nextEl: '.part-swiper-button-next',
-        prevEl: '.part-swiper-button-prev',
-      },
-    });
-  }
+    if(partSlider) {
+      let partitionsSlider = new Swiper(partSlider, {
+        slidesPerView: 'auto', // записать в пометки , как способ ограничивать контэйнер при свободном режиме при разной ширине слайдов, сэкономит кучу времени !!!!!!!!!!!!
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: '.part-swiper-pagination',
+          type: 'progressbar',
+        },
+        navigation: {
+          nextEl: '.part-swiper-button-next',
+          prevEl: '.part-swiper-button-prev',
+        },
+      });
+    }
+  });
 }();
