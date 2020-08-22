ce9ba2872065c5c0a4cefc1b84d096e975fb8354
Yuriy Bevov <jury19y@mail.ru>
2020-08-23 00:29:44 +0300

    phpmaller, отправка данных формы на почту

--- build/php/mail.php
+++ build/php/mail.php
@@ -1,10 +1,84 @@
 <?php
-    $fName = 'Имя: '.$_POST['name'].' <br />';
-    $fMail =  'Почта: '.$_POST['phone'].' <br />';
-    //$fMessage =  'Сообщение: '.$_POST['ask-us-text'].' <br />';
-    $AllInOne =  $fName.$fMail;//.$fMessage;
-    $to = 'jury19y@mail.ru';
-    $headers="From: Заявка с сайта Steel Balls\nContent-Type: text/html; charset=\"utf-8\"\n";
-    // функция, которая отправляет наше письмо
-    mail($to, 'Свяжитесь с нами', $AllInOne, $headers);
-?>
+  // Файлы phpmailer
+  require 'PHPMailer.php';
+  require 'SMTP.php';
+  require 'Exception.php';
+
+  // Переменные, которые отправляет пользователь
+  $name = $_POST['user_name'];
+  $phone = $_POST['user_pnone'];
+  $email = $_POST['user_mail'];
+  $text = $_POST['user_message'];
+  $file = $_FILES['user_file'];
+
+  if(!$name || $name.length < 0) {
+    $name = 'данные отсутствуют.';
+  }
+
+  if(!$email || $email.length < 0) {
+    $email = 'данные отсутствуют.';
+  }
+
+  if(!$text || $text.length < 0) {
+    $text = 'данные отсутствуют.';
+  }
+
+  // Формирование самого письма
+  $title = "Данные заявки: ";
+  $body = "
+    <b>Имя:</b> $name<br>
+    <b>Почта:</b> $email<br><br>
+    <b>Телефон:</b> $phone<br><br>
+    <b>Сообщение:</b><br>$text
+  ";
+
+  // Настройки PHPMailer
+  $mail = new PHPMailer;
+
+  try {
+      $mail->isSMTP();
+      $mail->CharSet = "UTF-8";
+      $mail->SMTPAuth   = true;
+      //$mail->SMTPDebug = 2;
+      $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+      // Настройки вашей почты
+      $mail->Host       = 'smtp.timeweb.ru'; // SMTP сервера вашей почты
+      $mail->Username   = 'stballs@cp06698.tmweb.ru'; // Логин на почте
+      $mail->Password   = 'kerqw623n1'; // Пароль на почте
+      $mail->SMTPSecure = 'ssl';
+      $mail->Port       = 465;
+      $mail->setFrom('stballs@cp06698.tmweb.ru', 'Новая заявка с сайта Steel Balls!'); // Адрес самой почты и имя отправителя
+
+      // Получатель письма
+      $mail->addAddress('jury19y@mail.ru');
+
+      // Прикрипление файлов к письму
+  if (!empty($file['name'][0])) {
+      for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+          $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+          $filename = $file['name'][$ct];
+          if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+              $mail->addAttachment($uploadfile, $filename);
+              $rfile[] = "Файл $filename прикреплён";
+          } else {
+              $rfile[] = "Не удалось прикрепить файл $filename";
+          }
+      }
+  }
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  // Проверяем отравленность сообщения
+  if ($mail->send()) {$result = "success";}
+  else {$result = "error";}
+
+  } catch (Exception $e) {
+      $result = "error";
+      $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+  }
+
+  // Отображение результата
+  echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
--- source/scripts/modules/forms/formSend.js
+++ source/scripts/modules/forms/formSend.js
@@ -6,6 +6,7 @@ const formSend = function (currentForm) {
 
   function success() {
     currentForm.reset();
+    console.log(currentForm.reset)
     modalState(thanksModal);
   }
 
@@ -34,53 +35,6 @@ const formSend = function (currentForm) {
     };
     xhr.send(data);
   };
-
-  /*function submitHandler(e) {
-    e.preventDefault();*/
-
-    /*var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      console.log("readyState=", this.readyState, "status=", this.status);
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-          // success, show this.responseText here
-          console.log("SUCCESS", this);
-      }
-    }
-
-    request.open(this.method, this.action, true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    var data = new FormData(this);
-    for (var key of data.keys())
-      console.log(key, data.get(key));
-
-    request.send(data);
-  }*/
-
-  /*document.querySelectorAll("form").forEach(form =>
-    form.addEventListener("submit", submitHandler)
-  );*/
-
-  /*console.log(currentForm)
-
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      console.log("readyState=", currentForm.readyState, "status=", currentForm.status);
-      if (currentForm.readyState === XMLHttpRequest.DONE && currentForm.status === 200) {
-          // success, show this.responseText here
-          console.log("SUCCESS", currentForm);
-      }
-    }
-
-    request.open(currentForm.method, currentForm.action, true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    var data = new FormData(currentForm);
-    for (var key of data.keys())
-      console.log(key, data.get(key));
-
-    request.send(data);*/
-
 }
 
 export {formSend};
