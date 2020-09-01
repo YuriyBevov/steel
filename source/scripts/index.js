53fde85a43920b479f7ab860baa766166597ae65
Yuriy Bevov <jury19y@mail.ru>
2020-09-01 19:46:04 +0300

    убрал ссылку до якоря

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -14,35 +14,3 @@ import fillUploadFile from './modules/fillUploadFile.js';
 import validatePhone from './plugins/phoneValidation.js';
 import submitForms from './modules/forms/submitForms.js';
 import openModal from './modules/modalLinks.js';
-import anchorScroll from './modules/anchorScroll.js';
-
-const btn = document.querySelector('.portfolio__btn');
-
-const photos = document.querySelectorAll('.portfolio__item');
-const step = 8;
-let firstPhoto = 0;
-let lastPhoto = step;
-const photosLength = photos.length;
-
-for (let i = lastPhoto; i < photos.length; i++) {
-  photos[i].style.display = 'none';
-}
-
-const onBtnClickHandler = () => {
-  for (let i = firstPhoto; i < lastPhoto; i++) {
-    photos[i].style.display = 'none';
-  }
-  firstPhoto += step;
-  lastPhoto += step;
-  if (lastPhoto >= photosLength) {
-    lastPhoto = photosLength;
-    btn.setAttribute('disabled', true);
-  }
-  for (let i = firstPhoto; i < lastPhoto; i++) {
-    photos[i].style.display = 'block';
-  }
-};
-
-if(btn) {
-  btn.addEventListener('click', onBtnClickHandler);
-}
